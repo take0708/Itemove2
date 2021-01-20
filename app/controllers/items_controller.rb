9a935e3682b38d0e171c76bc0d730088b1e84a10
take0708 <pk_er_stbo@yahoo.co.jp>
2021-01-21 08:31:47 +0900

    諸々(DB除く)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,46 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @items = Item.new
+  end
+
+  def create
+    @items = Item.new(item_params)
+    redirect_to root_path
+  end
+
+  def show
+    @items = Item.all
+  end
+
+  def edit
+    @items = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+    :code, 
+    :name,
+    :vol,
+  ) 
+  end
 end
